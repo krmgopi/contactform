28b66c5ad7f4f378298a76c58a9ce64fd2803825
Gopinath KRM <41666244+krmgopi@users.noreply.github.com>
2023-08-31 16:43:58 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,99 +1,63 @@
-
-
-
-<section class="luxary-trip-contact section-padding">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="col-md-2"> </div>
-                <div class="col-md-8">
-                    <div class="wrapper">
-                        <h3 class="contact-title text-uppercase fw-600 color-22 pos-relative playfair d-inline">Contact Us</h3>
-                        <div class="abform-area clearfix pos-relative">
-                            <form action="#" method="POST">
-                                <div class="abinput-group name float-left">
-
-                                    <input type="text" name="name" id="name3" placeholder="Name">
-                                </div>
-                                <div class="abinput-group phone float-left">
-
-                                    <input type="tel" name="phone" id="phone3" placeholder="Mobile Number" maxlength="10" onkeypress='if((event.keyCode < 46) || (event.keyCode > 57)) event.returnValue = false'>
-                                </div>
-
-                                <div class="abinput-group email float-left">
-
-                                    <input type="email" name="email" id="email3" placeholder="Email ID">
-                                </div>
-
-                                <div class="abinput-group subject float-left">
-
-                                    <input type="text" name="subject" id="sub3" placeholder="Subject">
-                                </div>
-
-
-                                <div class="abinput-group msg">
-
-                                    <textarea name="message" id="msg3" placeholder="Message"></textarea>
-                                </div>
-                                <button type="submit" class="text-uppercase fw-700 color-ff">Send Message</button>
-                            </form>
-                            <!--/form-->
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-2"> </div>
-            </div>
-            <!--/col-->
-        </div>
-    </div>
-</section>
-
 <?php
-if ((isset($_POST['name']) && !empty($_POST['name']))
-    &&   (isset($_POST['phone']) && !empty($_POST['phone']))
-    && (isset($_POST['email']) && !empty($_POST['email']))
-    && (isset($_POST['subject']) && !empty($_POST['subject']))
-) {
-    $name = $_POST['name'];
-    $phone = $_POST['phone'];
-    $email = $_POST['email'];
-    $subject = $_POST['subject'];
-    //$subject = "";
-    $message = $_POST['message'];
 
-    $to = $email;
-    $from = $email;
-    $fromName = $name;
+@$post = $_POST;
+
+//echo 'reached';
+
+$mail_from = $post['email'];
+$to = "gopinathkrm@gmail.com";
 
 
-    $htmlContent = ' 
-    <html> 
-    <head> 
-        <title>Welcome</title> 
-    </head> 
-    <body> 
-        <hr>
-        <h2>Dear ' . $name . '</h2> <br>
-        <h3>Thank you</h3> 
-        <h5>Regards</h3> 
+$yourname = $post['name'];
+$phone  = $post['phone'];
+$subject  = $post['subject'];
+$message  = $post['message'];
 
+
+$htmlContent = '
+    <html>
+    <head>
+    </head>
+    <body>
+        <table cellspacing="0" style="border: 2px solid #FB4314; width: 300px; height: 200px;">
+            <tr>
+                <th>Name:</th><td>'.$yourname.'</td>
+            </tr>
+            <tr style="background-color: #e0e0e0;">
+                <th>Email:</th><td>'.$mail_from.'</td>
+            </tr>
+            <tr style="background-color: #e0e0e0;">
+                <th>Phone:</th><td>'.$phone.'</td>
+            </tr>
+            <tr style="background-color: #e0e0e0;">
+                <th>Subject:</th><td>'.$subject.'</td>
+            </tr>
+             <tr style="background-color: #e0e0e0;">
+                <th>Message:</th><td>'.$message.'</td>
+            </tr>
+
+                        
+        </table>
     </body> 
     </html>';
 
-    // Set content-type header for sending HTML email 
-    $headers = "MIME-Version: 1.0" . "\r\n";
-    $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+$htmlContent;
 
-    // Additional headers 
-    $headers .= 'From: ' . $fromName . '<' . $from . '>' . "\r\n";
+// Set content-type header for sending HTML email
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+$headers .= "From:". $mail_from. "\r\n";
+$headers .= "Reply-To: ".$mail_from. "\r\n";
+$headers .= "Return-Path: ".$mail_from. "\r\n";
+$headers .= "CC: ".$to. "\r\n";
+$headers .= "BCC: ".$to. "\r\n";
+$headers.= 'Content-type: text/html; charset=iso-8859-1 \r\n';
 
-    // Send email 
-    if (mail($to, $subject, $htmlContent, $headers)) {
-        echo 'Email has sent successfully.';
-    } else {
-        echo 'Email sending failed.';
-    }
-
-}
-
+if ( mail($to,$subject,$htmlContent,$headers) ) {
+   echo "Mail sent successfully..";
+   } 
+   else 
+   {
+   echo "Error!!... Please try after sometime";
+   }
 ?>
